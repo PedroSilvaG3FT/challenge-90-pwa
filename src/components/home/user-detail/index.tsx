286d5c7a6a10b7ad6852a3275b658e04567385da
Pedro Silva <pedro.silva@cbyk.com.br>
2022-07-22 10:23:32 -0300

    summary control by store

--- src/components/home/user-detail/index.tsx
+++ src/components/home/user-detail/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import store from '@/store'
+import { useMapState } from '@/hooks'
 import { useRouter } from 'next/router'
 import Avatar from '@/assets/icons/avatar.png'
 import Summary from '@/components/home/user-detail/summary'
 import ScrollAnimation from '@/components/ui/scroll-animation'
 import UserGoals from '@/components/home/user-detail/user-goals'
+import { AuthStateInterface } from '@/store/@interfaces/authState.interface'
 import { Container, Content, Article, Image, Separator, Button } from './styles'
 
 const UserDetail: React.FC = () => {
     const router = useRouter()
-    const { user, isTypeChallenge } = store.getState().auth
+    const { user, isTypeChallenge } = useMapState('auth') as AuthStateInterface
 
     const goToProfile = () => router.push('/profile')
 
